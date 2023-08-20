6143fbb93deaad78facad4bcff4001cd7d2023b9
Vishal Soraganvi <99650261+VishalSoraganvi@users.noreply.github.com>
2023-08-20 14:28:07 +0530

    companion list push

---
+++ app/api/companion/route.ts
@@ -0,0 +1,37 @@
+import prismadb from "@/lib/prismadb";
+import { currentUser } from "@clerk/nextjs";
+import { NextResponse } from "next/server"
+
+export async function POST(req:Request){
+    try {
+        const {src, name, description, instructions, seed , categoryId} = await req.json();
+        const user = await currentUser();
+
+        if(!user || !user.id || !user.firstName){
+            return new NextResponse("Unauthorized" ,{status:401})
+        }
+
+        if(!src || !name || !description || !instructions || !seed || !categoryId){
+            return new NextResponse("Missing required fields" , {status: 400})
+        }
+        //todo : Check for subscription
+
+        const companion =  await prismadb.companion.create({
+            data:{
+                categoryId,
+                userId: user.id,
+                userName:user.firstName,
+                src,
+                name,
+                description,
+                instructions,
+                seed
+            }
+        })
+
+        return NextResponse.json(companion)
+    } catch (error) {
+        console.log("[COMPANION_POST]",error)
+        return new NextResponse("Internal error",{status:500})
+    }
+}
