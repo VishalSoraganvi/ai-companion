aaa49c9ffb0a9ebe2d7d4b165d5cf059a350a9d4
Vishal Soraganvi <99650261+VishalSoraganvi@users.noreply.github.com>
2023-08-20 14:28:56 +0530

    companion list

--- app/(root)/companion/[companionId]/components/companion-form.tsx
+++ app/(root)/companion/[companionId]/components/companion-form.tsx
@@ -4,6 +4,7 @@ import { Category, Companion } from "@prisma/client";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
 import {
   Form,
   FormControl,
@@ -26,6 +27,8 @@ import {
 import { Button } from "@/components/ui/button";
 import { Wand2 } from "lucide-react";
 import { Textarea } from "@/components/ui/textarea";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
 
 const PREAMBLE = `You are a fictional character whose name is Elon. You are a visionary entrepreneur and inventor. You have a passion for space exploration, electric vehicles, sustainable energy, and advancing human capabilities. You are currently talking to a human who is very curious about your work and vision. You are ambitious and forward-thinking, with a touch of wit. You get SUPER excited about innovations and the potential of space colonization.
 `;
@@ -84,11 +87,30 @@ export const CompanionForm = ({
       categoryId: undefined,
     },
   });
+  const { toast } = useToast();
+  const router = useRouter();
 
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    try {
+      if (initialData) {
+        //update companion functionality
+        await axios.patch(`/api/companion/${initialData.id}`, values);
+      } else {
+        await axios.post(`/api/companion`, values);
+      }
+      toast({
+        description: "Success!",
+      });
+      router.refresh();
+      router.push("/");
+    } catch (error) {
+      toast({
+        variant: "destructive",
+        description: "Something went wrong",
+      });
+    }
   };
 
   return (
---
+++ app/api/companion/[companionId]/route.ts
@@ -0,0 +1,44 @@
+import prismadb from "@/lib/prismadb";
+import { currentUser } from "@clerk/nextjs";
+import { NextResponse } from "next/server"
+
+export async function PATCH(req:Request,{params}:{params:{companionId:string}}){
+    try {
+        const {src, name, description, instructions, seed , categoryId} = await req.json();
+        const user = await currentUser();
+
+        if(!params.companionId){
+            return new NextResponse("Companion ID is required",{status:400})
+        }
+
+        if(!user || !user.id || !user.firstName){
+            return new NextResponse("Unauthorized" ,{status:401})
+        }
+
+        if(!src || !name || !description || !instructions || !seed || !categoryId){
+            return new NextResponse("Missing required fields" , {status: 400})
+        }
+        //todo : Check for subscription
+
+        const companion =  await prismadb.companion.update({
+            where:{
+                id:params.companionId
+            },
+            data:{
+                categoryId,
+                userId: user.id,
+                userName:user.firstName,
+                src,
+                name,
+                description,
+                instructions,
+                seed
+            }
+        })
+
+        return NextResponse.json(companion)
+    } catch (error) {
+        console.log("[COMPANION_PATCH]",error)
+        return new NextResponse("Internal error",{status:500})
+    }
+}
