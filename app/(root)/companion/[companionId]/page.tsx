01c6e159b996afe2b2e0bf6d28b634cdccbd677e
Vishal Soraganvi <99650261+VishalSoraganvi@users.noreply.github.com>
2023-08-21 12:39:40 +0530

    chat header commit

--- app/(root)/companion/[companionId]/page.tsx
+++ app/(root)/companion/[companionId]/page.tsx
@@ -1,6 +1,6 @@
 import prismadb from "@/lib/prismadb";
-import { AwardIcon } from "lucide-react";
 import { CompanionForm } from "./components/companion-form";
+import { auth, redirectToSignIn } from "@clerk/nextjs";
 
 interface CompanionIdPageProps {
   params: {
@@ -10,9 +10,16 @@ interface CompanionIdPageProps {
 const CompanionIdPage = async ({ params }: CompanionIdPageProps) => {
   //todo: Check for subscription
 
+  const {userId} =auth()
+
+  if(!userId){
+    return redirectToSignIn()
+  }
+
   const companion = await prismadb.companion.findUnique({
     where: {
       id: params.companionId,
+      userId
     },
   });
 
